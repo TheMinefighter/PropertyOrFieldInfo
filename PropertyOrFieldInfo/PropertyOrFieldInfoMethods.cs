e76eb9585e0a6b1f40c8d7b5293a711f77e2850b
Tobias Brohl <tobrohl@gmail.com>
2018-04-24 12:47:34 +0200

    Commit #1

---
+++ PropertyOrFieldInfo/PropertyOrFieldInfoMethods.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PropertyOrFieldInfo
+{
+   public static class PropertyOrFieldInfoMethods
+   {
+      public static IEnumerable<PropertyOrFieldInfo> DeclaredPropertiesAndFields(this TypeInfo src) => src.DeclaredProperties
+         .Select(x => new PropertyOrFieldInfo(x))
+         .Concat(src.DeclaredProperties.Select(x => new PropertyOrFieldInfo(x)));
+   }
+}
