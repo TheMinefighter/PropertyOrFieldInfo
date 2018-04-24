f725b5988515a1463c147dffe7291129b01807f9
Tobias Brohl <tobrohl@gmail.com>
2018-04-24 17:47:59 +0200

    ommented most stuff

--- PropertyOrFieldInfo/PropertyOrFieldInfo.cs
+++ PropertyOrFieldInfo/PropertyOrFieldInfo.cs
@@ -4,11 +4,31 @@ using System.Transactions;
 
 namespace PropertyOrFieldInfo
 {
-   public partial class PropertyOrFieldInfo : MemberInfo
+   public partial class PropertyOrFieldInfo : MemberInfo, IEquatable<PropertyOrFieldInfo>, IEquatable<PropertyInfo>, IEquatable<FieldInfo>
    {
+      /// <summary>
+      /// True if it is a <see cref="FieldInfo"/>, false if it is a <see cref="PropertyInfo"/>
+      /// </summary>
       public bool IsField { get;  }
+      /// <summary>
+      /// The actual <see cref="System.Reflection.MemberInfo"/>
+      /// </summary>
       public MemberInfo MemberInfo { get;  }
 
       private PropertyOrFieldInfo() { }
+      public bool Equals(PropertyOrFieldInfo other)
+      {
+         return MemberInfo.Equals(other.MemberInfo);
+      }
+
+      public bool Equals(PropertyInfo other)
+      {
+         return MemberInfo.Equals(other);
+      }
+
+      public bool Equals(FieldInfo other)
+      {
+         return MemberInfo.Equals(other);
+      }
    }
 }
--- PropertyOrFieldInfo/PropertyOrFieldInfoConversions.cs
+++ PropertyOrFieldInfo/PropertyOrFieldInfoConversions.cs
@@ -3,49 +3,75 @@ using System.Reflection;
 
 namespace PropertyOrFieldInfo {
 	public partial class PropertyOrFieldInfo {
-		public PropertyOrFieldInfo(PropertyInfo source) {
+      /// <summary>
+      /// Creates a new <see cref="PropertyOrFieldInfo"/> from a <see cref="PropertyInfo"/>
+      /// </summary>
+      /// <param name="source">The <see cref="PropertyInfo"/> to use</param>
+      public PropertyOrFieldInfo(PropertyInfo source) {
 			MemberInfo = source;
 			//IsField is false by default
 		}
-
+      /// <summary>
+      /// Creates a new <see cref="PropertyOrFieldInfo"/> from a <see cref="FieldInfo"/>
+      /// </summary>
+      /// <param name="source">The <see cref="FieldInfo"/> to use</param>
 		public PropertyOrFieldInfo(FieldInfo source) {
 			MemberInfo = source;
 			IsField = true;
 		}
-
-		public PropertyOrFieldInfo(MemberInfo src) {
-			switch (src.MemberType) {
+      /// <summary>
+      /// Creates a new <see cref="PropertyOrFieldInfo"/> from a <see cref="System.Reflection.MemberInfo"/>
+      /// </summary>
+      /// <param name="source">The MemberInfo to use</param>
+      /// <exception cref="ArgumentException">Thrown when the MemberInfo is not a <see cref="PropertyInfo"/> neither a <see cref="FieldInfo"/></exception>  
+      public PropertyOrFieldInfo(MemberInfo source) {
+			switch (source.MemberType) {
 				case MemberTypes.Field:
 					IsField = true;
 					break;
 				case MemberTypes.Property:
 					//IsField is false by default
 					break;
-				default: throw new ArgumentException("Not a PropertyInfo neither a FieldInfo");
+				default: throw new ArgumentException("Not a PropertyInfo neither a FieldInfo",nameof(source));
 			} 	
 
-			MemberInfo = src;
+			MemberInfo = source;
 		}
-
-		public static explicit operator PropertyInfo(PropertyOrFieldInfo src) {
-			if (!src.IsField) {
-				return (PropertyInfo) src.MemberInfo;
+      /// <summary>
+      /// Convertes a <see cref="PropertyOrFieldInfo"/> to a <see cref="PropertyInfo"/> if possible, throws otherwise
+      /// </summary>
+      /// <param name="source">The <see cref="PropertyOrFieldInfo"/> to convert</param>
+      /// <exception cref="InvalidOperationException">Thrown when it is a <see cref="FieldInfo"/> instead of the requested <see cref="PropertyInfo"/></exception>
+		public static explicit operator PropertyInfo(PropertyOrFieldInfo source) {
+			if (!source.IsField) {
+				return (PropertyInfo) source.MemberInfo;
 			}
 			else {
 				throw new InvalidOperationException("It´s a FieldInfo not a PropertyInfo");
 			}
 		}
-
-		public static explicit operator FieldInfo(PropertyOrFieldInfo src) {
-			if (src.IsField) {
-				return (FieldInfo) src.MemberInfo;
+      /// <summary>
+      /// Convertes a <see cref="PropertyOrFieldInfo"/> to a <see cref="FieldInfo"/> if possible, throws otherwise
+      /// </summary>
+      /// <param name="source">The <see cref="PropertyOrFieldInfo"/> to convert</param>
+      /// <exception cref="InvalidOperationException">Thrown when it is a <see cref="PropertyInfo"/> instead of the requested <see cref="FieldInfo"/></exception>
+      public static explicit operator FieldInfo(PropertyOrFieldInfo source) {
+			if (source.IsField) {
+				return (FieldInfo) source.MemberInfo;
 			}
 			else {
 				throw new InvalidOperationException("It´s a PropertyInfo not a FieldInfo");
 			}
 		}
-
-		public static explicit operator PropertyOrFieldInfo(FieldInfo src) => new PropertyOrFieldInfo(src);
-		public static explicit operator PropertyOrFieldInfo(PropertyInfo src) => new PropertyOrFieldInfo(src);
+	   /// <summary>
+	   /// Convertes a <see cref="FieldInfo"/> to a <see cref="PropertyOrFieldInfo"/>
+	   /// </summary>
+	   /// <param name="source">The <see cref="FieldInfo"/> to convert</param>
+      public static explicit operator PropertyOrFieldInfo(FieldInfo source) => new PropertyOrFieldInfo(source);
+      /// <summary>
+      /// Convertes a <see cref="PropertyInfo"/> to a <see cref="PropertyOrFieldInfo"/>
+      /// </summary>
+      /// <param name="source">The <see cref="PropertyInfo"/> to convert</param>
+      public static explicit operator PropertyOrFieldInfo(PropertyInfo source) => new PropertyOrFieldInfo(source);
 	}
 }
