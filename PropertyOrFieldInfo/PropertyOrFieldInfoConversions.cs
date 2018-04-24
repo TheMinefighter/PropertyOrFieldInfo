c82af28258dab0f9b12eeeab3d5047f59a77ce4b
Tobias Brohl <tobrohl@gmail.com>
2018-04-24 15:01:06 +0200

    Commit #3

---
+++ PropertyOrFieldInfo/PropertyOrFieldInfoConversions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Reflection;
+
+namespace PropertyOrFieldInfo {
+	public partial class PropertyOrFieldInfo {
+		public PropertyOrFieldInfo(PropertyInfo source) {
+			MemberInfo = source;
+			//IsField is false by default
+		}
+
+		public PropertyOrFieldInfo(FieldInfo source) {
+			MemberInfo = source;
+			IsField = true;
+		}
+
+		public PropertyOrFieldInfo(MemberInfo src) {
+			switch (src.MemberType) {
+				case MemberTypes.Field:
+					IsField = true;
+					break;
+				case MemberTypes.Property:
+					//IsField is false by default
+					break;
+				default: throw new ArgumentException("Not a PropertyInfo neither a Fieldinfo");
+			}
+
+			MemberInfo = src;
+		}
+
+		public static explicit operator PropertyInfo(PropertyOrFieldInfo src) {
+			if (!src.IsField) {
+				return (PropertyInfo) src.MemberInfo;
+			}
+			else {
+				throw new InvalidOperationException("It´s a FieldInfo not a PropertyInfo");
+			}
+		}
+
+		public static explicit operator FieldInfo(PropertyOrFieldInfo src) {
+			if (src.IsField) {
+				return (FieldInfo) src.MemberInfo;
+			}
+			else {
+				throw new InvalidOperationException("It´s a PropertyInfo not a Fieldinfo");
+			}
+		}
+
+		public static explicit operator PropertyOrFieldInfo(FieldInfo src) => new PropertyOrFieldInfo(src);
+		public static explicit operator PropertyOrFieldInfo(PropertyInfo src) => new PropertyOrFieldInfo(src);
+	}
+}
--- PropertyOrFieldInfo/PropertyOrFieldInfoMethods.cs
+++ PropertyOrFieldInfo/PropertyOrFieldInfoMethods.cs
@@ -1,13 +1,73 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Reflection;
 
-namespace PropertyOrFieldInfo
-{
-   public static class PropertyOrFieldInfoMethods
-   {
-      public static IEnumerable<PropertyOrFieldInfo> DeclaredPropertiesAndFields(this TypeInfo src) => src.DeclaredProperties
-         .Select(x => new PropertyOrFieldInfo(x))
-         .Concat(src.DeclaredProperties.Select(x => new PropertyOrFieldInfo(x)));
-   }
+namespace PropertyOrFieldInfo {
+	public partial class PropertyOrFieldInfo {
+		public bool CanRead {
+			get {
+				if (IsField) {
+					return ((FieldInfo) MemberInfo).IsPublic;
+				}
+				else {
+					if (!((PropertyInfo) MemberInfo).CanRead) {
+						return false;
+					}
+					else {
+						return ((PropertyInfo) MemberInfo).GetGetMethod().IsPublic;
+					}
+				}
+			}
+		}
+
+		public bool CanWrite {
+			get {
+				if (IsField) {
+					return ((FieldInfo) MemberInfo).IsPublic;
+				}
+				else {
+					if (!((PropertyInfo) MemberInfo).CanWrite) {
+						return false;
+					}
+					else {
+						return ((PropertyInfo) MemberInfo).GetSetMethod().IsPublic;
+					}
+				}
+			}
+		}
+
+		public Type GetType() {
+			if (IsField) {
+				return ((FieldInfo) MemberInfo).GetType();
+			}
+			else {
+				return ((PropertyInfo) MemberInfo).GetType();
+			}
+		}
+
+		public object GetValue(object reference) {
+			if (IsField) {
+				return ((FieldInfo) MemberInfo).GetValue(reference);
+			}
+			else {
+				return ((PropertyInfo) MemberInfo).GetValue(reference);
+			}
+		}
+
+		public void SetValue(object reference, object value) {
+			if (IsField) {
+				((FieldInfo) MemberInfo).SetValue(reference, value);
+			}
+			else {
+				((PropertyInfo) MemberInfo).SetValue(reference, value);
+			}
+		}
+
+		public override object[] GetCustomAttributes(bool inherit) => MemberInfo.GetCustomAttributes(inherit);
+		public override object[] GetCustomAttributes(Type attributeType, bool inherit) => MemberInfo.GetCustomAttributes(attributeType, inherit);
+		public override bool IsDefined(Type attributeType, bool inherit) => MemberInfo.IsDefined(attributeType, inherit);
+		public override Type DeclaringType => MemberInfo.DeclaringType;
+		public override MemberTypes MemberType => MemberInfo.MemberType;
+		public override string Name => MemberInfo.Name;
+		public override Type ReflectedType => MemberInfo.ReflectedType;
+	}
 }
